a8da03afdb4a88970498f0ae3518bf7f06b8def1
Chungphing Ly <chungphing@slash.co>
2020-04-03 18:32:58 +0700

    static header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Layout, Divider } from 'antd';
+import { PoweroffOutlined } from '@ant-design/icons';
 
-const Header = ({ title }) => (
-  <div>
-    <h1>{title}</h1>
-  </div>
+import AvatarPanel from '../AvatarPanel/AvatarPanel';
+import LanguageSwitcher from '../LanguageSwitcher/LanguageSwitcher';
+
+
+const { Header: AntHeader } = Layout;
+const Header = ({ image = '', name = 'John Doe' }) => (
+  <AntHeader className="flex flex-row items-center overflow-hidden  p-8" style={{ padding: 0 }}>
+    <h1 className="text-white text-xl mr-auto">COVID-19 Broadcasting System for The Kingdom of Cambodia</h1>
+    <LanguageSwitcher />
+    <AvatarPanel image={image} name={name} className="px-2" />
+    <Divider type="vertical" className="h-10" />
+    <PoweroffOutlined className="text-xl text-white px-2 cursor-pointer" />
+  </AntHeader>
 );
 
 export default Header;
