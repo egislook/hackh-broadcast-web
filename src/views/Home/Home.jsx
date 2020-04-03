580eca0adad2abea7a81e656994c265af8a20575
Chungphing Ly <chungphing@slash.co>
2020-04-03 15:40:35 +0700

    add tailwind and ant design

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Button } from 'antd';
 
 const Home = () => {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header text-gray-900">
         <p>
           Covid-19 Broadcast!
         </p>
-        <p>Hello world!</p>
+        <p className="text-2xl">Hello world!</p>
+        <Button type="primary">Button</Button>
+
       </header>
     </div>
   )
