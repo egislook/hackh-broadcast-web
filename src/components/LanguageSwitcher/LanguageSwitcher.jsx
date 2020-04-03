a5d0234876bfe59b1431dea077c42e57629c104c
VeasnapisethSlash <veasnapiseth@slash.co>
2020-04-03 22:54:17 +0700

    Add button

--- src/components/LanguageSwitcher/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher/LanguageSwitcher.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import i18next from "i18next";
 
-const LanguageSwitcher = () => (
-  <div className="langauge-switcher flex flex-row">
-    <div className="img-wrapper w-6 h-4 mx-1 border border-solid border-white rounded-sm cursor-pointer">
-      <img className="h-full w-full" src="/flag-cambodia.jpg" alt="Khmer" />
-    </div>
-    <div className="img-wrapper w-6 h-4 mx-1 border border-solid border-white rounded-sm cursor-pointer">
-      <img className="h-full w-full" src="/flag-british.jpg" alt="English" />
-    </div>
-  </div>
-);
+const changeLanguage = (lang) => {
+  console.log('lang', lang);
+  i18next.changeLanguage(lang);
+};
+
+const LanguageSwitcher = () => {
+    return(
+        <div className="langauge-switcher flex flex-row">
+            <div className="img-wrapper w-6 h-4 mx-1 border border-solid border-white rounded-sm cursor-pointer" onClick={() => changeLanguage('km')}>
+                <img className="h-full w-full" src="/flag-cambodia.jpg" alt="Khmer"/>
+            </div>
+            <div className="img-wrapper w-6 h-4 mx-1 border border-solid border-white rounded-sm cursor-pointer" onClick={() => changeLanguage('en')}>
+                <img className="h-full w-full" src="/flag-british.jpg" alt="English"/>
+            </div>
+        </div>
+    );
+};
 
 export default LanguageSwitcher;
--- src/localization/khmer.js
+++ src/localization/khmer.js
@@ -2,8 +2,9 @@ export default {
   translation: {
     'Welcome to React': 'សូមស្វាគមន៍មកកាន់ React',
     app_title: 'ប្រព័ន្ធផ្សព្វផ្សាយ COVID-19 នៃ​ព្រះ​រាជាណាចក្រ​កម្ពុជា',
-    logout_title: 'logout from system?',
-    cancel_button: 'Cancel',
-    send_button: 'Send',
+    logout_title: 'ចាកចេញពីកម្មវិធី?',
+    cancel_button: 'បោះបង់',
+    send_button: 'ផ្ញើសារ',
+    ok_button: 'យល់ព្រម'
   },
 };
