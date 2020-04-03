39afc96b0f2c531dff626bb8b0212a574b730570
Chungphing Ly <chungphing@slash.co>
2020-04-03 18:47:03 +0700

    add translation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Layout, Divider, Modal } from 'antd';
 import { PoweroffOutlined } from '@ant-design/icons';
+import { useTranslation } from 'react-i18next';
+
 
 import AvatarPanel from '../AvatarPanel/AvatarPanel';
 import LanguageSwitcher from '../LanguageSwitcher/LanguageSwitcher';
 
-
 const { confirm } = Modal;
 
-const showConfirm = () => {
+const showConfirm = (title) => {
   confirm({
-    title: 'Do you Want to logout from the system?',
+    title,
     // content: 'Some descriptions',
     onOk() {
       console.log('OK');
@@ -23,14 +24,18 @@ const showConfirm = () => {
 
 
 const { Header: AntHeader } = Layout;
-const Header = ({ image = '', name = 'John Doe' }) => (
-  <AntHeader className="flex flex-row items-center overflow-hidden  p-8" style={{ padding: 0 }}>
-    <h1 className="text-white text-xl mr-auto">COVID-19 Broadcasting System for The Kingdom of Cambodia</h1>
-    <LanguageSwitcher />
-    <AvatarPanel image={image} name={name} className="px-2" />
-    <Divider type="vertical" className="h-10" />
-    <PoweroffOutlined className="text-xl text-white px-2 cursor-pointer" onClick={showConfirm} />
-  </AntHeader>
-);
+const Header = ({ image = '', name = 'John Doe' }) => {
+  const { t } = useTranslation();
+
+  return (
+    <AntHeader className="flex flex-row items-center overflow-hidden  p-8" style={{ padding: 0 }}>
+      <h1 className="text-white text-xl mr-auto">{t('app_title')}</h1>
+      <LanguageSwitcher />
+      <AvatarPanel image={image} name={name} className="px-2" />
+      <Divider type="vertical" className="h-10" />
+      <PoweroffOutlined className="text-xl text-white px-2 cursor-pointer" onClick={() => showConfirm(t('logout_title'))} />
+    </AntHeader>
+  );
+};
 
 export default Header;
