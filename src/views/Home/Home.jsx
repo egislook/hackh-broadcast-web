ee106fb4993f46acb410ebdf4229ce28ec9d31df
Chungphing Ly <chungphing@slash.co>
2020-04-03 14:22:50 +0700

    configure project

---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Covid-19 Broadcast!
+        </p>
+        <p>Hello world!</p>
+      </header>
+    </div>
+  )
+}
+
+export default Home
