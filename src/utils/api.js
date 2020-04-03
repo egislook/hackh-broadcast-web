66bfbb435739e0f3faea8e0533421573a8c5ffdf
Chungphing Ly <chungphing@slash.co>
2020-04-03 20:28:03 +0700

    mock api data

--- src/utils/api.js
+++ src/utils/api.js
@@ -9,8 +9,20 @@ const instance = axios.create({
 
 
 const sendMessage = (options) => instance.post('/send', options);
+const fetchMessages = (options) => instance.get('/messages', options);
+const fetchUserInfo = (options) => instance.get('/me', options);
+const login = (options) => instance.post('/login', options);
+const logout = (options) => instance.post('/logout', options);
+const requestOtp = (options) => instance.post('/request-otp', options);
+const authenticateOtp = (options) => instance.post('/authenticate-otp', options);
 
 export default {
   fetchConfirmedCases,
   sendMessage,
+  fetchMessages,
+  fetchUserInfo,
+  login,
+  logout,
+  requestOtp,
+  authenticateOtp,
 };
