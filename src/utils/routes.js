feacf179caa16c14fe9e507457b6acabbbfa3f87
VeasnapisethSlash <veasnapiseth@slash.co>
2020-04-03 17:24:27 +0700

    Add login to route

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,11 +1,13 @@
 // Misc routes
 import NotFound from '../components/NotFound/NotFound';
 import Home from '../views/Home/Home';
+import Login from '../views/Login/Login';
 // Register Routes
 
 // Application routes
 const routes = [
   { path: '/', component: Home, exact: true },
+  { path: '/login', component: Login, exact: true },
   { component: NotFound },
 ];
 
