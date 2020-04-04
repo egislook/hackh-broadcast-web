b560ce8135d850dcb6b64c6b3b7f82f03ac90d2d
VeasnapisethSlash <veasnapiseth@slash.co>
2020-04-04 10:02:28 +0700

    Add login form

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,11 +1,12 @@
 // Misc routes
 import NotFound from '../components/NotFound/NotFound';
+import Login from '../views/Login/Login';
 import Home from '../views/Home/Home';
 // Register Routes
 
 // Application routes
 const routes = [
-  { path: '/', component: Home, exact: true },
+  { path: '/', component: Login, exact: true },
   { component: NotFound },
 ];
 
--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { Form, Icon, Input, Button, Card } from 'antd';
+import './Login.css'
 
-const Login = () => (
-  <div className="App">
-    <header className="App-header">
-      <p>
-        Covid-19 Login!
-      </p>
-      <p>Hello world!</p>
-    </header>
-  </div>
-);
+
+class Login extends Component {
+
+    render() {
+        return (
+            <div className="login-content">
+                <Card hoverable className="content flex login-main">
+                    <Form className="login-form">
+                        <Form.Item>
+                            <p className="login-title">COVID-19 Broadcast</p>
+                        </Form.Item>
+                        <Form.Item>
+                            <Input
+                                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                placeholder="Username"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Input
+                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                type="password"
+                                placeholder="Password"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <div style={{ textAlign: 'right' }}>
+                                <Button type="primary" htmlType="submit" className="login-form-button" style={{ textAlign: 'right'}}>
+                                    Log in
+                                </Button>
+                            </div>
+                        </Form.Item>
+                    </Form>
+                </Card>
+            </div>
+        );
+    }
+}
 
 export default Login;
