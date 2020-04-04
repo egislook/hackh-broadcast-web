0cb7b44e1e13b51b662c5cf722b86bcb462099dd
Chungphing Ly <chungphing@slash.co>
2020-04-04 14:57:32 +0700

    add alert and translation

---
+++ src/utils/constants.js
@@ -0,0 +1,22 @@
+export const TELEGRAM = {
+  key: 'telegram',
+  label: 'Telegram',
+  maxChar: 4096,
+  icon: '/telegram.svg',
+};
+
+export const MESSENGER = {
+  key: 'messenger',
+  label: 'Messenger',
+  maxChar: 300,
+  icon: '/messenger.svg',
+  template: '',
+};
+
+export const SMS = {
+  key: 'sms',
+  label: 'SMS',
+  maxChar: 300,
+  icon: '/sms.svg',
+  template: '',
+};
