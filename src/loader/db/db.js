f45538446546c93f34b239e69ff2f44d529d5be2
VeasnapisethSlash <veasnapiseth@slash.co>
2020-04-05 13:42:49 +0700

    Fix eslint

--- src/loader/db/db.js
+++ src/loader/db/db.js
@@ -1,5 +1,5 @@
 import { database } from 'firebase/app';
-import moment from "moment";
+import moment from 'moment';
 
 export const fetchAllMessage = async () => {
   const databaseRef = database().ref('telegram').limitToLast(50);
@@ -7,8 +7,8 @@ export const fetchAllMessage = async () => {
     databaseRef.on('value', (snapshot) => {
       const data = snapshot.val() || {};
       const messages = [];
-      snapshot.forEach(child => {
-        messages.push(child.val())
+      snapshot.forEach((child) => {
+        messages.push(child.val());
       });
       // it should trigger every time there are some changes happening
       return resolve(messages.reverse());
@@ -21,8 +21,6 @@ export const postMessage = async (options) => {
   const result = await databaseRef.push({
     message: options.message,
     date: moment().format(),
-  }).then((snap) => {
-    return snap.key;
-  });
+  }).then((snap) => snap.key);
   return result;
 };
--- src/views/OTP/OTP.jsx
+++ src/views/OTP/OTP.jsx
@@ -1,24 +1,24 @@
-import React, {Component} from 'react';
-import IndexNavbar from "../../components/Navbars/IndexNavbar.js";
-import DemoFooter from "../../components/Footers/DemoFooter.js";
+import React, { Component } from 'react';
+import IndexNavbar from '../../components/Navbars/IndexNavbar.js';
+import DemoFooter from '../../components/Footers/DemoFooter.js';
 
-import SectionOTP from "../index-sections/SectionOTP.jsx";
-import SectionDownload from "../index-sections/SectionDownload.jsx";
+import SectionOTP from '../index-sections/SectionOTP.jsx';
+import SectionDownload from '../index-sections/SectionDownload.jsx';
 
 
 class OTP extends Component {
-    render () {
-        return (
-            <>
-                <IndexNavbar />
-                <div className="main">
-                    <SectionOTP />
-                    <SectionDownload />
-                    <DemoFooter />
-                </div>
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <IndexNavbar />
+        <div className="main">
+          <SectionOTP />
+          <SectionDownload />
+          <DemoFooter />
+        </div>
+      </>
+    );
+  }
 }
 
 export default OTP;
